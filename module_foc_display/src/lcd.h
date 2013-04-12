92cb61358da61dfba820fda6eada2dc50c6c0f5e
beaumontm <markb@xmos.com>
2013-04-12 12:53:04 +0100

    Legalised lcd.xc, now legalised all S/W in module_foc_display

--- module_foc_display/src/lcd.h
+++ module_foc_display/src/lcd.h
@@ -15,17 +15,21 @@
 #ifndef _LCD_H_
 #define _LCD_H_
 
+#include <xs1.h>
 #include <xccompat.h>
+#include <assert.h>
+#include <print.h>
 
-#define CHAR_BUF_SIZE	21*4
+#define FONT_WIDTH 5
+#define CHAR_BUF_SIZE	(21 * 4)
 
 #ifdef __XC__
 	/** \brief The control structure for the LCD ports.
-         *
-         *  The display uses an I2C interface with an extra control signal
-         *  to support selection between a data read/write and a control
-         *  read/write.  This extra signal is bit zero of the p_core1_shared
-         *  member - which contains no other signals despite the name.
+	 *
+	 *  The display uses an I2C interface with an extra control signal
+	 *  to support selection between a data read/write and a control
+	 *  read/write.  This extra signal is bit zero of the p_core1_shared
+	 *  member - which contains no other signals despite the name.
 	 */
 	typedef struct LCD_INTERFACE_TAG
 	{
@@ -34,27 +38,15 @@
 		out port p_lcd_cs_n;      //!< i2c chip select
 		out port p_core1_shared;  //!< Display data/control select
 	} LCD_INTERFACE_TYP;
-#endif
+#endif // __XC__
 
-/** \brief Reverse the order of bytes in the array
- *
- * \param s the byte array to reverse
- */
-void reverse(char s[]);
-
-/** \brief Convert an integer into a base 10 ASCII string
- *
- * \param n the integer to represent in string form
- * \param s the output byte array to contain the number
- */
-void itoa(int n, char s[]);
-
+/*****************************************************************************/
 /** \brief Initialise the LCD device
  *
  * \param p the LCD interface description
  */
 void lcd_ports_init( REFERENCE_PARAM(LCD_INTERFACE_TYP, p) );
-
+/*****************************************************************************/
 /** \brief Write a byte to the LCD
  *
  * \param p the LCD interface description
@@ -62,13 +54,13 @@ void lcd_ports_init( REFERENCE_PARAM(LCD_INTERFACE_TYP, p) );
  * \param is_data a boolean indicating if the write is data or control information
  */
 void lcd_byte_out( REFERENCE_PARAM(LCD_INTERFACE_TYP, p), unsigned char i, int is_data );
-
+/*****************************************************************************/
 /** \brief Clear the LCD
  *
  * \param p the LCD interface description
  */
 void lcd_clear( REFERENCE_PARAM(LCD_INTERFACE_TYP, p) );
-
+/*****************************************************************************/
 /** \brief Draw an image on the LCD
  *
  *  Draw an image on the LCD.  The image is assumed to cover the complete
@@ -78,7 +70,7 @@ void lcd_clear( REFERENCE_PARAM(LCD_INTERFACE_TYP, p) );
  * \param p the LCD interface description
  */
 void lcd_draw_image( const unsigned char image[], REFERENCE_PARAM(LCD_INTERFACE_TYP, p) );
-
+/*****************************************************************************/
 /** \brief Write text to a row on the LCD
  *
  * Display a row of text. The LCD columns beyond the end of the string will be
@@ -89,19 +81,20 @@ void lcd_draw_image( const unsigned char image[], REFERENCE_PARAM(LCD_INTERFACE_
  * \param p the LCD interface description
  */
 void lcd_draw_text_row( const char string[], int lcd_row, REFERENCE_PARAM(LCD_INTERFACE_TYP, p) );
-
+/*****************************************************************************/
 /** \brief Macro for writing data to the LCD
  *
  * \param p the LCD interface description
  * \param i the data to write
  */
 #define lcd_data_out(p, i)         	lcd_byte_out(p, i, 1)
-
+/*****************************************************************************/
 /** \brief Macro for writing a control byte to the LCD
  *
  * \param p the LCD interface description
  * \param i the data to write
  */
 #define lcd_comm_out(p, i)         	lcd_byte_out(p, i, 0)
+/*****************************************************************************/
 
 #endif /* _LCD_H_ */
